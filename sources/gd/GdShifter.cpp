baf73a44f43f1613f0ba38ad889dd594f2e209f2
Jean Pierre Cimalando <jp-dev@inbox.ru>
2021-08-23 19:52:24 +0200

    Add TODO warning

--- sources/gd/GdShifter.cpp
+++ sources/gd/GdShifter.cpp
@@ -7,3 +7,7 @@
 #elif GD_USE_SUPERCOLLIDER_PITCH_SHIFTER
 #   include "shifters/GdShifterSuperCollider.cpp"
 #endif
+
+#if !GD_SHIFTER_CAN_REPORT_LATENCY
+#   pragma message("TODO: implement latency reporting for this pitch shifter")
+#endif
